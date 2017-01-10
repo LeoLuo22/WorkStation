28a6e65693889f2874c795d53e68586c14ff3cd0
Leo <leoluo22@gmail.com>
2017-01-10 18:22:01 +0800

    Chd_lib spider with requests: Complete

--- Spiders/chdlib_requests/books.py
+++ Spiders/chdlib_requests/books.py
@@ -4,18 +4,22 @@
 import os
 import collections
 import lxml
-import pymango
+import pymongo
 import requests
 import random
+import time
 from bs4 import BeautifulSoup
 from requests.exceptions import MissingSchema
+from utils import file
+import url
+import bson
 
 BASE_URL = "http://wiscom.chd.edu.cn:8080/opac/item.php"
 HEAD = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
 }
 FILEPATH = 'e:/books/'
 
-client = pymango.MongoClient("mongodb://localhost:27017/")
+client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client.chd
 collection = db.library
 
@@ -24,10 +28,20 @@ class Library():
         pass
 
     def __get_douban_info(self, isbn):
+        """Get book's info from douban
+        @param isbn
+            Book's isbn
+        @return
+            A dict that contains douban info
+        """
         info_dict = {}
         url = 'http://wiscom.chd.edu.cn:8080/opac/ajax_douban.php'
         payload = {'isbn': isbn}
-        response = requests.get(url, params=payload, headers=HEAD)
+        try:
+            response = requests.get(url, params=payload, headers=HEAD)
+        except requests.exceptions.ConnectionError:
+            time.sleep(10)
+            return self.__get_douban_info(isbn)
         infos = eval(response.text) #eval function can convert str object to dict object
         for key, value in infos.items():
             while '\\' in value:
@@ -41,7 +55,10 @@ class Library():
         @return: type: str; value: isbn
         """
         isbn_raw = soup[2].dd.string
-        isbn = isbn_raw.split('/')[0]
+        try:
+            isbn = isbn_raw.split('/')[0]
+        except AttributeError:
+            return 0
         while '-' in isbn:
             isbn = isbn.replace('-', '')
         return isbn
@@ -52,49 +69,104 @@ class Library():
         @book: book's information
         """
         book = collections.OrderedDict()
-        response = requests.get(BASE_URL, params=payload, headers=HEAD)
+        try:
+            response = requests.get(BASE_URL, params=payload, headers=HEAD)
+        except requests.exceptions.ConnectionError:
+            time.sleep(10)
+            return self.parse(payload)
         base = response.content.decode('utf-8')
         soup = BeautifulSoup(base, 'lxml')
         item_detail_soup = soup.find_all('dl', attrs={'class': 'booklist'})
         isbn = self.__get_isbn(item_detail_soup)
-        douban_info = self.__get_douban_info(isbn)
+        if isbn:
+            douban_info = self.__get_douban_info(isbn)
         for detail in item_detail_soup:
             if detail.dt.string:
                 book[detail.dt.string.replace(':', '')] = detail.dd.get_text()
-        book.update(douban_info)#update: insert douban_info to book those are not in book
-        hasImg = self.__save_image(book.get('image'), book.get('题名/责任者').replace('/', '_'))
+        if isbn:
+            book.update(douban_info)#update: insert douban_info to book those are not in book
+        book.update(payload)
+        try:
+            hasImg = self.__save_image(book.get('image'), book.get('题名/责任者').replace('/', '_'))
+        except AttributeError:
+            return 1
         if hasImg:
             book['pic'] = FILEPATH + book.get('题名/责任者').replace('/', '_')
-        collection.insert(book)
+        try:
+            collection.insert(book)
+        except pymongo.errors.ServerSelectionTimeoutError:
+            print('MongoDB server is not activated, try again. ')
+            return 0
+        except bson.errors.InvalidDocument:
+            return 1
         #print(book)
         print("{0} 已添加".format(book.get('题名/责任者')))
+        return 1
 
     def __save_image(self, link, name):
         """Write image to disk
-        @param link: Image's url on Douban
-        @param name: book's name and author
-        @return: None
+        @param link
+            Image's url on Douban
+        @param name
+            book's name and author
+        @return
+            None
         """
+        name = name.replace('>', '').replace('<', '')
+        name = name.replace(':', '')
         try:
             response = requests.get(link, headers=HEAD)
         except MissingSchema:
-            print("{0} 没有封面图片".format(name))
+            #print("{0} 没有封面图片".format(name))
+            return 0
+        except requests.exceptions.ConnectionError:
             return 0
         cwd = os.getcwd()
         os.chdir('e:/books/')
-        with open(name+'.jpg', 'wb') as f:
-            f.write(response.content)
+        try:
+            with open(name+'.jpg', 'wb') as f:
+                f.write(response.content)
+        except OSError:
+            with open(name[0: 2]+'.jpg', 'wb') as f:
+                f.write(response.content)
         os.chdir(cwd)
         return 1
 
-def read_url_from_file():
-    with open('unusedUrls.txt', 'r') as f:
-        for line in f:
-            with open('usedUrls.txt', 'a') as fh:
-                fh.write(line)
-            yield line.replace('\n', '')
+def read_url_from_file(file, last_record, used_name):
+    """Read marc_no from file
+    @param file
+        The file contains all marc_nos
+    @param last_record
+        Last used  marc_no, generally saved in used_name file
+    @param used_name
+        Filename that is to save used marc_nos
+    @return
+        Literator, marc_no
+    """
+    totals = []
+    news = []
+    flag = False
 
-def main():
+    with open(file, 'r') as f:
+        for line in f:
+            line = line.replace('\n', '')
+            totals.append(line)
+
+    for total in totals:
+        if total == last_record:
+            flag = True
+        if flag:
+            news.append(total)
+
+    news.remove(last_record)
+
+    with open(used_name+'.txt', 'a') as fh:
+        for new in news:
+            fh.write(new+'\n')
+            yield new
+
+
+def main(last_record):
     """Where the module start
     @payload: book's id
     """
@@ -106,13 +178,32 @@ def main():
     """
     payload = {}
     library = Library()
-    a = read_url_from_file()
+    a = read_url_from_file('unusedUrls.txt', last_record, 'used1')
     while True:
         #print(next(a))
-        payload['marc_no'] = next(a)
-        library.parse(payload)
+        try:
+            payload['marc_no'] = next(a)
+        except StopIteration:
+            unusedUrls = []
+            with open('unusedUrls.txt', 'r') as fd:
+                for line in fd:
+                    unusedUrls.append(line.replace('\n', ''))
+            pay = {}
+            last_record = unusedUrls[len(unusedUrls)-1]
+            pay['marc_no'] = last_record
+            book = url.BookSpider()
+            try:
+                book.get_urls(pay)
+            except RecursionError:
+                return main(last_record)
+            return main(last_record)
+        flag = library.parse(payload)
+        if not flag:
+            break
     return
+
+    #file.remove_duplicate('merged.txt', 'usedUrls.txt', 'merged1.txt')
 
 
 if __name__ == "__main__":
-    main()
+    main('0000065249')
--- Spiders/chdlib_requests/url.py
+++ Spiders/chdlib_requests/url.py
@@ -1,7 +1,19 @@
+import requests
+from bs4 import BeautifulSoup
+import lxml
+import random
+import time
+
+HEAD = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'
+}
 
 class BookSpider():
     def __init__(self):
         self.unusedUrls = []
+        with open('unusedUrls.txt', 'r') as fh:
+            for line in fh:
+                line = line.replace('\n', '')
+                self.unusedUrls.append(line)
 
     def get_urls(self, payload):
         """Discover more urls
@@ -9,7 +21,11 @@ class BookSpider():
         @return
         """
         ajax_url = 'http://wiscom.chd.edu.cn:8080/opac/ajax_likehood_book.php'
-        response = requests.get(ajax_url, params=payload, headers=HEAD)
+        try:
+            response = requests.get(ajax_url, params=payload, headers=HEAD)
+        except requests.exceptions.ConnectionError:
+            time.sleep(10)
+            return self.get_urls({'marc_no': self.unusedUrls[random.randint(0, len(self.unusedUrls)-1)]})
         soup = BeautifulSoup(response.text, 'lxml')
         hrefs = soup.find_all('a', attrs={'target': '_blank'})
         for href in hrefs:
@@ -19,5 +35,14 @@ class BookSpider():
                 with open('unusedUrls.txt', 'a') as f:
                     f.write(marc_no+'\n')
                 self.unusedUrls.append(marc_no)
-        print(len(self.unusedUrls))
-        #self.get_urls({'marc_no': self.unusedUrls[random.randint(0, len(self.unusedUrls)-1)]})
+        self.get_urls({'marc_no': self.unusedUrls[random.randint(0, len(self.unusedUrls)-1)]})
+
+def main():
+    book = BookSpider()
+    payload = {}
+    payload['marc_no'] = '0000378068'
+    book.get_urls(payload)
+
+if __name__ == '__main__':
+    main()
+
