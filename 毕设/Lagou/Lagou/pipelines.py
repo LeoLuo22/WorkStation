29fdcf12741032f6e19b245ec6dcab3495cb65fe
Leo <leoluo22@gmail.com>
2017-02-19 09:03:24 +0800

    Lagou Spider: Complete pipeline.py

--- 毕设/Lagou/Lagou/pipelines.py
+++ 毕设/Lagou/Lagou/pipelines.py
@@ -4,8 +4,30 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
-
+import pymongo
 
 class LagouPipeline(object):
+
+    collection_name = 'job'
+
+    def __init__(self, mongo_url, mongo_db):
+        self.mongo_url = mongo_url
+        self.mongo_db = mongo_db
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+                   mongo_url = crawler.settings.get('MONGO_URL'),
+                   mongo_db = crawler.settings.get('MONGO_DATABASE', 'items')
+                   )
+
+    def open_spider(self, spider):
+        self.client = pymongo.MongoClient(self.mongo_url)
+        self.db = self.client[self.mongo_db]
+
+    def close_spider(self, spider):
+        self.client.close()
+
     def process_item(self, item, spider):
+        self.db[self.collection_name].insert(dict(item))
         return item
