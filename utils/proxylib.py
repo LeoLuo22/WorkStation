6cc300cbe4b7462ec04e312510f23265fa168bb0
Leo <leoluo22@gmail.com>
2017-02-23 10:25:56 +0800

    Proxylib: Complte get available proxies from database

--- utils/proxylib.py
+++ utils/proxylib.py
@@ -2,6 +2,8 @@
 """
 import requests
 import pymysql
+from user_agents import UserAgents
+import random
 from bs4 import BeautifulSoup
 from datetime import datetime
 import lxml
@@ -13,7 +15,7 @@ starturl = 'http://www.kuaidaili.com/free/inha/1/'
 URLS = []
 
 class MySql(object):
-    def __init__(self,  user, passwd, db=None, host='127.0.0.1', port=3306):
+    def __init__(self,  user=None, passwd=None, db=None, host='127.0.0.1', port=3306):
         """Initialize a connection
         @param user
          username
@@ -25,14 +27,14 @@ class MySql(object):
         self.config = {
         'host': host,
         'port': port,
-        'user': user,
-        'password': passwd,
+        'user': 'Leo',
+        'password': 'mm123456',
         'db': 'utils',
         'charset': 'utf8mb4',
         'cursorclass': pymysql.cursors.DictCursor,
         }
 
-        self.proxies = []
+        #self.proxies = []
         self.connection = pymysql.connect(**self.config)
         self.cursor = self.connection.cursor()
 
@@ -76,60 +78,49 @@ class MySql(object):
             print('错误: ',err)
             return
         results = self.cursor.fetchall()
+
         self.connection.commit()
+        #self.__close()
 
-        proxy = []
-        for result in results:
-            proxy = result.get('protocol') + '://' + result.get('host') + ':' + result.get('port')
-            self.proxies.append(proxy)
-
-        self.__close()
+        return results
 
     def update(self, host, available):
         """Update data to database
         """
         sql = "UPDATE proxies SET available = '{available}' , lastcheck = '{datetime}' WHERE host = '{host}'".format(available=available, host=host, datetime=datetime.now())
+
         self.cursor.execute(sql)
         self.connection.commit()
-        self.__close()
+        #self.__close()
 
 
     def __close(self):
         self.cursor.close()
         self.connection.close()
 
-def getUrl():
-    for i in range(1, 200):
-        url = starturl.replace("1", str(i))
-        r = requests.get(url, headers=HEADER)
-        rst = wash(r.text)
-        if rst:
-            print(rst)
-            with open('proxies.txt', 'a') as fh:
-                fh.write(rst + '\n')
-    return
-
-def wash(urlRaw):
-    ips = []
-    ports = []
-    urlSoup = BeautifulSoup(urlRaw, 'lxml')
-    ipsRaw = urlSoup.find_all('td', attrs={'data-title': 'IP'})
-    portsRaw = urlSoup.find_all('td', attrs={'data-title': 'PORT'})
-
-    for i in range(len(ipsRaw)):
-        url = "http://" + ipsRaw[i].get_text() + ":" + portsRaw[i].get_text()
-        if test(url):
-            return url
-        return
-
-def test(url):
-    proxy = {'http': url}
+def wash(protocol, host, port):
+    """Test if it can connect to Bing
+    """
+    proxy = {}
+    url = protocol + '://' + host + ':' + port
+    proxy[protocol] = url
+    print('Testing ', url, end='')
+    #print(proxy)
+    header = {}
+    header['User-Agent'] = random.choice(UserAgents)
+    #print(header)
     try:
-        r = requests.get('https://www.baidu.com', proxies=proxy, headers=HEADER, timeout=2)
-    except Exception:
+        response = requests.get('https://www.bing.com', headers=header, timeout=1, proxies=proxy)
+    except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
+        print('......fail')
         return False
-    if r.status_code == 200:
+    soup = BeautifulSoup(response.text, 'lxml')
+    bing = soup.find('div', attrs={'class': 'hp_sw_logo hpcLogoWhite'})
+    #print(bing.get_text())
+    if bing.get_text() == '必应':
+        print('......ok')
         return True
+    print('......fail')
     return False
 
 def addr_split(addr):
@@ -161,12 +152,23 @@ def read_from_file(filename):
             rst.append(line.replace('\n', ''))
 
     return rst
+def get_avaiable():
+    mysql = MySql()
+    proxies = []
+    proxy = ''
+    results = mysql.retrieve('protocol', 'port', 'host', available=1)
+    for result in results:
+        proxy = result.get('protocol') + '://' + result.get('host') + ':' + result.get('port')
+        proxies.append(proxy)
+
+    return proxies
 
 def main():
-    mysql = MySql('Leo', 'mm123456')
-    #mysql.retrieve('protocol', 'port', 'host', available=1)
-    #print(mysql.proxies)
-    mysql.update('106.46.136.27', 1)
+    print(get_avaiable())
+    #print(proxies)
+    #wash(**mysql.proxies[1])
+    #print(random.choice(UserAgents))
+    #mysql.update('106.46.136.27', 1)
 
     """
     with open('proxies.txt', 'r') as fh:
--- utils/user_agents.py
+++ utils/user_agents.py
@@ -2,9 +2,7 @@
     Usage:
     UserAgents.pc_agents -> list of agents
 """
-class UserAgents(object):
-    def __init__(self):
-        self.pc_agents = [
+UserAgents = [
     "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; AcooBrowser; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
     "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.0; Acoo Browser; SLCC1; .NET CLR 2.0.50727; Media Center PC 5.0; .NET CLR 3.0.04506)",
     "Mozilla/4.0 (compatible; MSIE 7.0; AOL 9.5; AOLBuild 4337.35; Windows NT 5.1; .NET CLR 1.1.4322; .NET CLR 2.0.50727)",
